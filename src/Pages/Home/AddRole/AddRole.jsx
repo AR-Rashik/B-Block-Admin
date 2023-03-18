01d1b178eff016f33e1df6e2a247d04ff29c7bc4
AR-Rashik <66801782+AR-Rashik@users.noreply.github.com>
2023-03-18 20:30:06 +0600

    Responsive new sidebar with pages route

---
+++ src/Pages/Home/AddRole/AddRole.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddRole = () => {
+  return (
+    <div>
+      <h1>This is add role page</h1>
+    </div>
+  );
+};
+
+export default AddRole;
--- src/Routes/Routes/Routes.jsx
+++ src/Routes/Routes/Routes.jsx
@@ -1,7 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
 import AddClient from "../../Pages/Home/AddClient/AddClient";
+import AddRole from "../../Pages/Home/AddRole/AddRole";
 import AllClients from "../../Pages/Home/AllClients/AllClients";
+import AllRoles from "../../Pages/Home/AllRoles/AllRoles";
+import BlockList from "../../Pages/Home/BlockList/BlockList";
+import ChangePassword from "../../Pages/Home/ChangePassword/ChangePassword";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
 
@@ -15,12 +19,28 @@ const router = createBrowserRouter([
         element: <Home></Home>,
       },
       {
-        path: "addclient",
+        path: "/client/addclient",
         element: <AddClient></AddClient>,
       },
       {
-        path: "/allclients",
+        path: "/client/allclients",
         element: <AllClients></AllClients>,
+      },
+      {
+        path: "/role/addrole",
+        element: <AddRole></AddRole>,
+      },
+      {
+        path: "/role/allroles",
+        element: <AllRoles></AllRoles>,
+      },
+      {
+        path: "/settings/changepassword",
+        element: <ChangePassword></ChangePassword>,
+      },
+      {
+        path: "/settings/blocklist",
+        element: <BlockList></BlockList>,
       },
     ],
   },
